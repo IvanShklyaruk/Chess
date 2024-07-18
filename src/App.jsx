ff994abc6b18cabb46733c8f79219042685760a0
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-18 02:38:01 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ChessPage from "./pages/ChessPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <ChessPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
